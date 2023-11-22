fd1efb4c2ed3d7c30bcfa41a2d141127f62cc740
pickleginderick <ginderick@gmail.com>
2023-11-22 21:39:44 +0800

    update LogConfig

--- index.js
+++ index.js
@@ -25,17 +25,77 @@ class LogLevel {
 }
 
 class Logger {
+  #config;
+
+  constructor(log_config) {
+    log_config = log_config || LogConfig.with_defaults();
+    LogConfig.assert(log_config);
+    this.#config = log_config;
+  }
+}
+
+class LogConfig {
   #level = LogLevel.Info;
 
-  constructor(log_level) {
-    if (arguments.length > 0) {
-      LogLevel.assert(log_level);
-      this.#level = log_level;
+  #rolling_config;
+
+  #file_prefix = "Logtar_";
+
+  static assert(log_config) {
+    if (arguments.length > 0 && !(log_config instanceof LogConfig)) {
+      throw new Error(
+        `log_config must be an instance of LogConfig. Unsupported param ${JSON.stringify(
+          log_config,
+        )}`,
+      );
     }
+  }
+
+  static with_defaults() {
+    return new LogConfig();
+  }
+
+  with_log_level(log_level) {
+    LogLevel.assert(log_level);
+    this.#level = log_level;
+    return this;
+  }
+
+  with_rolling_config(rolling_config) {
+    this.#rolling_config = RollingConfig.from_json(config);
+  }
+
+
+  /****
+  *
+  * @param {string}
+  * @return {LogConfig} The current instance of LogConfig
+  * @throws {Error} If the file_prefix is not a string.
+  *
+  * /
+  with_file_prefix(file_prefix) {
+    if (typeof file_prefix !== "string") {
+      throw new Error(
+        `file_prefix must be a string. Unsupported param ${JSON.stringify(
+          file_prefix,
+        )}`,
+      );
+    }
+
+    this.#file_prefix = file_prefix;
+    return this;
   }
 
   get level() {
     return this.#level;
+  }
+
+  get rolling_config() {
+    return this.#rolling_config;
+  }
+
+  get file_prefix() {
+    return this.#file_prefix;
   }
 }
 
