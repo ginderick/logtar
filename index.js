8d2da11c3c5bbf24064d17c88abedc6079a97322
pickleginderick <ginderick@gmail.com>
2023-11-23 21:39:15 +0800

    update Logger class

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require("node:fs");
+
 class LogLevel {
   static Debug = 0;
   static Info = 1;
@@ -25,12 +27,41 @@ class LogLevel {
 }
 
 class Logger {
+  /**
+   *
+   * @type {LogConfig}
+   *
+   *
+   **/
   #config;
+
+  /**
+   *
+   * @returns {Logger} A new instance of Logger with default values
+   * @description the default log level is `LogLevel.Info`
+   */
+  static with_defaults() {
+    return new Logger();
+  }
+
+  /**
+   * @param {LogConfig} log_config The log config to be used
+   * @return {Logger} A new instance of Logger with the specified log config.
+   * @throws {Error} If the log_config is not an instance of LogConfig
+   *
+   */
+  static with_config(log_config) {
+    return new Logger(log_config);
+  }
 
   constructor(log_config) {
     log_config = log_config || LogConfig.with_defaults();
     LogConfig.assert(log_config);
     this.#config = log_config;
+  }
+
+  get level() {
+    return this.#config.level;
   }
 }
 
@@ -55,14 +86,41 @@ class LogConfig {
     return new LogConfig();
   }
 
+  static from_json(json) {
+    let log_config = new LogConfig();
+
+    Object.keys(json).forEach((key) => {
+      switch (key) {
+        case "level":
+          log_config = log_config.with_log_level(json[key]);
+          break;
+        case "rolling_config":
+          log_config = log_config.with_rolling_config(json[key]);
+          break;
+        case "file_prefix":
+          log_config = log_config.with_file_prefix(json[key]);
+          break;
+      }
+    });
+
+    return log_config;
+  }
+
+  static from_file(file_path) {
+    const file_contents = fs.readFileSync(file_path);
+
+    return LogConfig.from_json(JSON.parse(file_contents));
+  }
+
   with_log_level(log_level) {
     LogLevel.assert(log_level);
     this.#level = log_level;
     return this;
   }
 
-  with_rolling_config(rolling_config) {
+  with_rolling_config(config) {
     this.#rolling_config = RollingConfig.from_json(config);
+    return this;
   }
 
   /**
@@ -217,4 +275,11 @@ class RollingConfig {
 module.exports = {
   Logger,
   LogLevel,
+  LogConfig,
+  RollingConfig,
+  RollingSizeOptions,
+  RollingTimeOptions,
 };
+
+const config = LogConfig.from_file("./config.demo.json");
+const logger = Logger.with_config(config);
