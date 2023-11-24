6755f991da9204147db824087e4274e19ca762d7
pickleginderick <ginderick@gmail.com>
2023-11-24 20:52:37 +0800

    output logs

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 const { Logger, LogConfig } = require("./index");
 
 const logger = Logger.with_config(LogConfig.from_file("./config.json"));
+
+logger.debug("Hello debug");
+logger.info("Hello info");
+logger.warn("Hello warning");
+logger.error("Hello error");
+logger.critical("Hello critical");
